677cbc374643f24a889259323afa3fed87616e34
chethan v <chethan2patil@gmail.com>
2020-03-08 07:18:07 +0530

    description view is pending

--- index.js
+++ index.js
@@ -1,89 +1,122 @@
 var categories = [];
 var recipes = [];
 
-!async function(){
+!async function () {
     let data = await fetch("http://temp.dash.zeta.in/food.php")
         .then((response) => response.json())
         .then(data => {
-             categories = data.categories;
+            categories = data.categories;
             recipes = data.recipes;
+            createFavorites(recipes);
             createCategory(categories);
             createRecipesList(recipes);
-            return categories,recipes ;
+            return categories, recipes;
         })
         .catch(error => {
             console.error(error);
         });
-    
-    console.log(recipes);    
+}();
 
-    }();
-console.log(recipes, categories); 
- function searchRecipes() {
-    
-    let inputValue = document.getElementById("autocomplete-input").value;
-    var searchTerm = inputValue;
-    console.log(inputValue);
-    let matchedTerms = [];
-    searchTerm = searchTerm.toLowerCase();
-    // -----------------ans1--------------------
-    matchedTerms = recipes.filter(function(i) {
-        return i.name.toLowerCase().indexOf(searchTerm) > -1;
-    });
-    createRecipesList(matchedTerms);
- }
+let timeout = null;
+function searchRecipes() {
+
+    // delay one sec before search such that wont have to make autocomplete every key stroke
+    clearTimeout(timeout);
+    timeout = setTimeout(function () {
+        let inputValue = document.getElementById("autocomplete-input").value;
+        var searchTerm = inputValue;
+        console.log(inputValue);
+        let matchedTerms = [];
+        searchTerm = searchTerm.toLowerCase();
+        matchedTerms = recipes.filter(function (i) {
+            return (i.name.toLowerCase() && i.category.toLowerCase() .indexOf(searchTerm) > -1) ;
+        });
+        createRecipesList(matchedTerms);
+    }, 1000);
+}
 
 function createCategory(categoriesList) {
-
-    const categoriesElement = document.getElementById("categoriesDivId");
-
-
+    console.log(categoriesList);
+    const categoriesElement = document.getElementById("CategoriesDivId");
     // categories view
     for (let i = 0; i < categoriesList.length; i++) {
-       
-        let icon = document.createElement("i");
-        icon.className = "material-icons";
-        icon.innerHTML = "cake";
         let aElement = document.createElement("a");
-        aElement.className = "categoriesDiv";
-        aElement.id = categoriesList[i].name;
-        aElement.innerText = categoriesList[i].name;
-        aElement.appendChild(icon);
+        aElement.className = "categories-a";
+       // aElement.href = searchRecipes(categoriesList[i].name);
+        let categoryElement = document.createElement("div");
+        categoryElement.className = "category";
+        let imgElement = document.createElement("img");
+        imgElement.src = categoriesList[i].image;
+        let textElement = document.createElement("div");
+        textElement.innerText = categoriesList[i].name;
+        categoryElement.appendChild(imgElement);
+        categoryElement.appendChild(textElement);
+        aElement.appendChild(categoryElement);
         categoriesElement.appendChild(aElement);
-       
     }
 }
-function createRecipesList (recipesList){
+
+function createFavorites(recipesList) {
+    let favRecipeList = recipesList.filter((recipe) => recipe.isFavourite);
+    let recipesListElement = createRecipeCard(favRecipeList, "favourite");
+    const recipesListDiv = document.getElementById("FavRecipesList");
+    recipesListDiv.appendChild(recipesListElement);
+}
+
+function createRecipesList(recipesList) {
+    // remove old cards in the dom
+    console.log(recipesList);
+    document.getElementById("recipesList").innerHTML = "";
     const recipesListDiv = document.getElementById("recipesList");
-    //recipesList view
+    let recipesListElement;
+    if (recipesList.length <= 0) {
+        recipesListElement = document.createElement("div");
+        recipesListElement.className = "no-dish";
+        recipesListElement.innerHTML = "NO DISH";
+    } else {
+        recipesListElement = createRecipeCard(recipesList, "category");
+    }
+    recipesListDiv.appendChild(recipesListElement);
+}
+
+function createRecipeCard(recipesList, recipeType) {
     let ulElement = document.createElement("ul");
     ulElement.className = "recipesUl"
     for (let i = 0; i < recipesList.length; i++) {
-
         let liElement = document.createElement("li");
-
         let imageNode = document.createElement("img");
         imageNode.src = recipesList[i].image;
         imageNode.id = "recipesImage";
         imageNode.className = recipesList[i].category;
         liElement.appendChild(imageNode);
-        let titleNode = document.createElement("p");
+        let descriptionNode = document.createElement("div");
+        descriptionNode.className = "description";
+
+        let descriptionRightNode = document.createElement("div");
+        let titleNode = document.createElement("h4");
         titleNode.innerHTML = recipesList[i].name;
-        titleNode.className = "categoryName";
-        liElement.appendChild(titleNode);
-        let spanCartNode = document.createElement("span");
-        let addToCart = document.createElement("button");
-        addToCart.className = "waves-effect waves-light btn-small";
-        addToCart.innerHTML = "ADD TO BAG";
-        spanCartNode.appendChild(addToCart);
-        titleNode.appendChild(spanCartNode);
-        let priceNode = document.createElement("p");
+        titleNode.className = "category-name";
+        descriptionRightNode.appendChild(titleNode);
+        let priceNode = document.createElement("div");
+        priceNode.className = "price";
         priceNode.innerHTML = "&#x20b9;" + recipesList[i].price;
-        liElement.appendChild(priceNode);
+        descriptionRightNode.appendChild(priceNode);
+
+        let descriptionLeftNode = document.createElement("div");
+        let addToCart = document.createElement("button");
+        addToCart.className = "add-to-cart";
+        if (recipeType == "favourite") {
+            addToCart.innerHTML = "REORDER";
+        } else {
+            addToCart.innerHTML = "ADD TO BAG";
+        }     
+        
+        descriptionLeftNode.appendChild(addToCart);
+
+        descriptionNode.appendChild(descriptionRightNode);
+        descriptionNode.appendChild(descriptionLeftNode);
+        liElement.appendChild(descriptionNode);
         ulElement.appendChild(liElement);
     }
-    recipesListDiv.appendChild(ulElement);
-
- 
-
-}
+    return ulElement;
+};
